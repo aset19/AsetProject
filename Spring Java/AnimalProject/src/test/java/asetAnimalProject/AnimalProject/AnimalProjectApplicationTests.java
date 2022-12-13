15adbdc99357f0835171e85c17acd65ccfc37676
Aset <vip.askenov@mail.ru>
2022-12-13 14:19:24 +0300

    FirstSpringProject

---
+++ Spring Java/AnimalProject/src/test/java/asetAnimalProject/AnimalProject/AnimalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package asetAnimalProject.AnimalProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
