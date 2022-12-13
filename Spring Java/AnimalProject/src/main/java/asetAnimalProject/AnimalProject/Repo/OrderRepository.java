8c69633995d50983dccb5542fdaa664e0664e832
Aset <vip.askenov@mail.ru>
2022-12-13 14:24:56 +0300

    Revert "FirstSpringProject"
    This reverts commit 15adbdc99357f0835171e85c17acd65ccfc37676.

--- Spring Java/AnimalProject/src/main/java/asetAnimalProject/AnimalProject/Repo/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package asetAnimalProject.AnimalProject.Repo;
-
-import asetAnimalProject.AnimalProject.Entity.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrderRepository  extends JpaRepository<Order, Long> {
-}
